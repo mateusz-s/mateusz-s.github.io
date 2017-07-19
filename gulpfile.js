8b7cee36c10191c0b2d1724e44e5a80310eee739
Mateusz Smykowski <msmykowski@interia.pl>
2017-07-19 22:26:09 +0200

    Gulp task configurations

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('sass/*.scss')
+                .pipe(sass({outputStyle: 'compressed'}))
+                .pipe(gulp.dest('css'))
+                .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("js/*.js").on('change', browserSync.reload);
+});
