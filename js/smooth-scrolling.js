2210a55c5f4c78fb7cca6ce48751cd92df9d3040
Mateusz Smykowski <msmykowski@interia.pl>
2017-07-24 20:22:43 +0200

    Add bugfix for Mozilla Firefox

--- js/smooth-scrolling.js
+++ js/smooth-scrolling.js
@@ -1,6 +1,6 @@
-$(document).on('click', 'a[href^="#"]', function(event){
+$(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
-    $('body').animate({
+    $('**html, body**').animate({ // Bugfix jQuery animate scrolling for Mozilla Firefox
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
 });
